3b74eefc8d5274505bdc2cbbfd1c439a7f75133f
HassanShanJava <hassanshan675@gmail.com>
2025-11-03 06:33:01 +0500

    feat: update layout and page components to include LightRays background effect and enhance page content with a new heading and description.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <h1>Welcome</h1>
+    <section>
+
+      <h1 className='text-center'>The Hub for Every Dev <br/> Event You Can't Miss</h1>
+      <p className='text-center mt-5'>Hackathon, Conference, Meetup, and more, All in One Place</p>
+    </section>
   )
 }
 
